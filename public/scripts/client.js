a75964d683d868202e9ff0a871a4a5a3a8b7d292
NoahNovick <nnovickfaille1@gmail.com>
2020-02-25 18:54:21 -0500

    add days since and function to take in tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,59 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac"
+      },
+      content: {
+        text:
+          "If I have seen further it is by standing on the shoulders of giants"
+      },
+      created_at: 1461116232227
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd"
+      },
+      content: {
+        text: "Je pense , donc je suis"
+      },
+      created_at: 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      $("#tweet-list").append(createTweetElement(tweet));
+    }
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  };
+
+  const createTweetElement = function(tweet) {
+    let $tweet = $("<article>").addClass("tweet");
+    const html = `<header>
+    <div>
+    <img src="${tweet.user.avatars}">
+  <span>${tweet.user.name}</span>
+    </div>
+<span class="appear">${tweet.user.handle}</span>
+</header>
+<main>${tweet.content.text}</main>
+<footer>
+<span>${Math.round((Date.now() - tweet.created_at) / 86400000)} days ago</span>
+<span>share</span>
+</footer>`;
+    $tweet.append(html);
+    return $tweet;
+  };
+
+  renderTweets(data);
+});
