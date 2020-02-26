a5e7a9c1034158c9717ddc8b1925f962bb6bc618
NoahNovick <nnovickfaille1@gmail.com>
2020-02-26 14:43:54 -0500

    prepend tweets to load in order and clear textarea after submition

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,44 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-  const data = [];
-
-  $("#new-tweet").submit(function(event) {
-    event.preventDefault();
-    $.ajax({
-      url: "/tweets/",
-      type: "POST",
-      data: $("textarea").serialize()
-    });
-    loadTweets();
-  });
-  const loadTweets = function() {
-    $.ajax({
-      url: "/tweets",
-      type: "GET",
-      data: "JSON",
-      success: response => {
-        const renderedTweets = renderTweets(response);
-        return renderedTweets;
-      }
-    });
-  };
-
-  const renderTweets = function(tweets) {
-    $("#tweet-list").empty();
-    for (let tweet of tweets) {
-      $("#tweet-list").append(createTweetElement(tweet));
+const loadTweets = function() {
+  $.ajax({
+    url: "/tweets",
+    type: "GET",
+    data: "JSON",
+    success: response => {
+      const renderedTweets = renderTweets(response);
+      return renderedTweets;
     }
-  };
+  });
+};
+const renderTweets = function(tweets) {
+  $("#tweet-list").empty();
+  for (let tweet of tweets) {
+    $("#tweet-list").prepend(createTweetElement(tweet));
+  }
+};
 
-  const createTweetElement = function(tweet) {
-    let $tweet = $("<article>").addClass("tweet");
-    const html = `<header>
-    <div>
-    <img src="${tweet.user.avatars}">
-  <span>${tweet.user.name}</span>
-    </div>
+const createTweetElement = function(tweet) {
+  let $tweet = $("<article>").addClass("tweet");
+  const html = `<header>
+  <div>
+  <img src="${tweet.user.avatars}">
+<span>${tweet.user.name}</span>
+  </div>
 <span class="appear">${tweet.user.handle}</span>
 </header>
 <main>${tweet.content.text}</main>
@@ -49,9 +36,22 @@ $(document).ready(function() {
 <span>${Math.round((Date.now() - tweet.created_at) / 86400000)} days ago</span>
 <span>share</span>
 </footer>`;
-    $tweet.append(html);
-    return $tweet;
-  };
+  $tweet.append(html);
+  return $tweet;
+};
 
-  renderTweets(data);
+$(document).ready(function() {
+  loadTweets();
+  $("#new-tweet").submit(function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: "/tweets/",
+      type: "POST",
+      data: $("textarea").serialize(),
+      success: function() {
+        loadTweets();
+        $("textarea").val("");
+      }
+    });
+  });
 });
