015a24440f1183cf30ca00861a498ca666b61707
NoahNovick <nnovickfaille1@gmail.com>
2020-02-26 14:15:19 -0500

    add tweets to page using get and post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,7 @@
  */
 
 $(document).ready(function() {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac"
-      },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants"
-      },
-      created_at: 1461116232227
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd"
-      },
-      content: {
-        text: "Je pense , donc je suis"
-      },
-      created_at: 1461113959088
-    }
-  ];
+  const data = [];
 
   $("#new-tweet").submit(function(event) {
     event.preventDefault();
@@ -44,14 +20,16 @@ $(document).ready(function() {
     $.ajax({
       url: "/tweets",
       type: "GET",
-      data: "JSON"
-    }).then(response => {
-      const renderedTweets = renderTweets(response.data);
-      return renderedTweets;
+      data: "JSON",
+      success: response => {
+        const renderedTweets = renderTweets(response);
+        return renderedTweets;
+      }
     });
   };
 
   const renderTweets = function(tweets) {
+    $("#tweet-list").empty();
     for (let tweet of tweets) {
       $("#tweet-list").append(createTweetElement(tweet));
     }
